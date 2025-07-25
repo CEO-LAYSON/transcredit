a18ad65246c964c5decb5ae9d904a8bb60c59e61
Layson Ndenza <laysonndenza@gmail.com>
2025-07-25 13:52:52 +0300

    added scrollTotop function of the routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import PartnershipForm from "./pages/partnershipApplication";
 import NotFoundPage from "./pages/notFoundPage";
+import ScrollToTop from "./components/ScrollToTop";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         {/* Public Routes */}
         <Route path="/" element={<HomePage />} />
